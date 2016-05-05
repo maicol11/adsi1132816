9352f0feab71ddca42a99c7ac681194e9db6322e
maicol11 <maicol11ce@live.com>
2016-05-05 12:40:34 -0500

    ejercicios

---
+++ html/html/codigo.js
@@ -0,0 +1,10 @@
+var nombre = prompt("Cual es su nombre?");
+
+if(nombre=="oliver")
+{
+		alert("Bienvenido: "+nombre+" el maincraftiano");
+} 
+else
+{
+		alert("Bienvenido: "+nombre+" el maincraftiano");
+}
